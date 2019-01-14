e3d1548223a0610269fd2d9eb51e7f9c9ce45969
Radoslav Gerganov <rgerganov@vmware.com>
2019-01-14 13:36:25 +0200

    Add Baby-step Giant-step implementation

--- src/java/dlp/DLP.java
+++ src/java/dlp/DLP.java
@@ -2,6 +2,8 @@ package dlp;
 
 import java.io.FileInputStream;
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -34,7 +36,7 @@ public class DLP implements Runnable {
             }
             System.out.printf("Testing from %d to %d\n", loc_start, end);
             BigInteger curr = g.modPow(BigInteger.valueOf(loc_start), p);
-            for (int i = loc_start ; i < end ; i++) {
+            for (int i = loc_start; i < end; i++) {
                 if (A.equals(curr)) {
                     System.out.printf("solution found, a=%d\n", i);
                     System.exit(2);
@@ -44,32 +46,66 @@ public class DLP implements Runnable {
         }
     }
 
+    void bruteForce(int threads) throws InterruptedException {
+        System.out.printf("Solving g^a mod p = A using brute force with %d threads\n\n", threads);
+        Thread[] t = new Thread[threads];
+        for (int i = 0; i < t.length; i++) {
+            t[i] = new Thread(this);
+            t[i].start();
+        }
+        for (int i = 0; i < t.length; i++) {
+            t[i].join();
+        }
+    }
+
+    void babyStepGiantStep() {
+        System.out.println("\nSolving g^a mod p = A using Baby-step Giant-step algorithm\n");
+        int m = (int) Math.ceil(Math.sqrt(Integer.MAX_VALUE));
+        Map<BigInteger, Integer> table = new HashMap<BigInteger, Integer>();
+        BigInteger tmp = BigInteger.valueOf(1);
+        for (int i = 0; i < m; i++) {
+            table.put(tmp, i);
+            tmp = tmp.multiply(g).mod(p);
+        }
+        BigInteger gi = g.modInverse(p);
+        BigInteger factor = gi.modPow(BigInteger.valueOf(m), p);
+        BigInteger e = A;
+        for (int i = 0; i < m; i++) {
+            if (table.containsKey(e)) {
+                int a = i * m + table.get(e);
+                System.out.printf("solution found, a=%d\n", a);
+                break;
+            }
+            e = e.multiply(factor).mod(p);
+        }
+    }
+
     public static void main(String args[]) throws Exception {
         if (args.length < 1) {
-            System.err.println("Usage: java dlp.DLP <conf_file>");
+            System.err.println("Usage: java dlp.DLP [--fast] <conf_file>");
             System.exit(1);
         }
-        DLP dlp = new DLP();
+        boolean fast = false;
         Properties props = new Properties();
-        props.load(new FileInputStream(args[0]));
+        if (args[0].equals("--fast")) {
+            props.load(new FileInputStream(args[1]));
+            fast = true;
+        } else {
+            props.load(new FileInputStream(args[0]));
+        }
+        DLP dlp = new DLP();
         dlp.g = new BigInteger(props.getProperty("g"), 16);
         System.out.println("g=" + dlp.g.toString(16));
         dlp.p = new BigInteger(props.getProperty("p"), 16);
         System.out.println("p=" + dlp.p.toString(16));
         dlp.A = new BigInteger(props.getProperty("A"), 16);
         System.out.println("A=" + dlp.A.toString(16));
-        dlp.start.set(Integer.valueOf(props.getProperty("start")));
-        int threads = Integer.valueOf(props.getProperty("threads"));
-
-        System.out.printf("Solving g^a mod p = A with %d threads\n\n", threads);
-
-        Thread[] t = new Thread[threads];
-        for (int i = 0 ; i < t.length ; i++) {
-            t[i] = new Thread(dlp);
-            t[i].start();
-        }
-        for (int i = 0 ; i < t.length ; i++) {
-            t[i].join();
+        if (fast) {
+            dlp.babyStepGiantStep();
+        } else {
+            dlp.start.set(Integer.valueOf(props.getProperty("start")));
+            int threads = Integer.valueOf(props.getProperty("threads"));
+            dlp.bruteForce(threads);
         }
     }
 }
